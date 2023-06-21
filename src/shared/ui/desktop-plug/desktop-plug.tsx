9cd28289d8f22dcced1d70a4a21fd6d7ab6e18cb
eepodrezov <egor.podrezov@kirarock.team>
2023-06-21 16:32:58 +0300

    edit desktop plug

--- src/shared/ui/desktop-plug/desktop-plug.tsx
+++ src/shared/ui/desktop-plug/desktop-plug.tsx
@@ -13,7 +13,7 @@ export const DesktopPlug: FCWithClassName<DesktopPlugProps> = ({ width, height, 
       data-testid='image-no-photo'
       className={cn('flex items-center justify-center croogla-text text-text rounded-large', className)}
       // Из-за JIT компилятора не получится всунуть в cn через tailwind
-      style={{ width: width || '100%', height: height || '100%' }}
+      style={{ width: width || '100%', height: height || '100vh' }}
     >
       <p className='w-[276px] flex flex-col gap-5 items-center'>
         <InDevelopmentPlugIcon className='mb-small stroke-black' />
